64fdc22db8f9835ce01b7ae690c071215451294e
Gabriel Loutfalla Kanawati <gabrielkanawati3@gmail.com>
2023-04-02 03:10:48 -0300

    update build tokens

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 02 Apr 2023 06:10:36 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGrey200": "#c1c1c4",
+  "ColorGrey500": "#50505c",
+  "ColorGrey800": "#1c1c1f",
+  "ColorGrey900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
